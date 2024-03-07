30d9dfce01eaa4dfd08526d2e93e09c52bfd4bd3
Varunjaishreeram <varunbharadwaj444@gmail.com>
2024-03-07 16:58:55 +0530

    add api

--- index.js
+++ index.js
@@ -20,9 +20,14 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({ ipaddress, language, software });
 });
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
